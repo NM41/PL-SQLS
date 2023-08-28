c84127807f9838e3d61bd27b998df7354be6ed43
NM41 <nayanparmar2190@gmail.com>
2023-08-28 15:12:37 +0530

    make 6 plsql basic start query

---
+++ neon_plsql_file.sql
@@ -0,0 +1,69 @@
+----------------------------------------
+------------ PL/SQL --------------------
+----------------------------------------
+
+-- SET SERVEROUTPUT ON;
+
+-- 1. HELLO WORLD
+-- DECLARE
+-- BEGIN
+--     DBMS_OUTPUT.PUT_LINE('HELLO WORLD');
+-- END;
+
+-- 2. Print value of variable
+-- SET SERVEROUTPUT ON;
+-- DECLARE
+--     A INTEGER := 10;
+-- BEGIN
+--     DBMS_OUTPUT.PUT_LINE(A);
+-- END;
+
+-- 3. ADDITION
+-- SET SERVEROUTPUT ON;
+
+-- DECLARE
+--     A INTEGER := 10;
+--     B INTEGER := 20;
+--     C INTEGER;
+-- BEGIN
+--     C := A + B;
+--     DBMS_OUTPUT.PUT_LINE(C);
+-- END;
+
+-- 4. SUBSTRACTION
+-- SET SERVEROUTPUT ON;
+
+-- DECLARE
+--     A INTEGER := 30;
+--     B INTEGER := 10;
+--     C INTEGER;
+-- BEGIN
+--     C := A-B;
+--     DBMS_OUTPUT.PUT_LINE(C);
+-- END;
+
+-- 5. MULTIPLICATION
+-- SET SERVEROUTPUT ON;
+
+-- DECLARE
+--     A INTEGER := 10;
+--     B INTEGER := 15;
+--     C INTEGER;
+-- BEGIN
+--     C := A * B;
+--     DBMS_OUTPUT.PUT_LINE(C);
+-- END;
+
+-- 6. DIVISION
+-- SET SERVEROUTPUT ON;
+
+-- DECLARE
+--     A INTEGER := 20;
+--     B INTEGER := 10;
+--     C INTEGER;
+-- BEGIN
+--     C:= A/B;
+--     DBMS_OUTPUT.PUT_LINE(C);
+-- END;
+
+COMMIT
